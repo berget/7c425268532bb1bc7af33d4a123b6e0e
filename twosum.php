205ad4f80cd21920bc56a5dbd555f8f9ac73b578
dark <darkrewrite@gmail.com>
2018-06-14 01:51:12 +0800

    finish

---
+++ twosum.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * LeetCode 1. Two Sum
+ *
+ * @authors Your Name (you@example.org)
+ * @date    2018-06-14 01:33:24
+ * @version $Id$
+ */
+
+function twoSum($nums, $target) {
+	$fruit = count($nums);
+
+	foreach ($nums as $key => $first_num) {
+		$ans_num = $target - $first_num;
+		$flag = $key + 1;
+
+		for ($i=$flag; $i < $fruit; $i++) {
+			if ($ans_num == $nums[$i]) {
+				$ans = array($key, $i);
+        return $ans;
+			}
+		}
+	}
+}
